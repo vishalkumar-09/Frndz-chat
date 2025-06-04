5942a9e08f2e78c3806e83dd9058ff4e34847e5c
Vishal Kumar Gaud <iamvishalkumar09@gmail.com>
2025-06-05 03:06:52 +0530

    Our Codebase get optimized

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,13 @@
 import express from  "express"
 import "dotenv/config";
 
-const app = express()
-const PORT = process.env.PORT
+import authRoutes from "./routes/auth.route.js";
 
-app.get("/api/auth/signup",(req,res)=>{
-    res.send("Signup Route");
-});
+const app = express();
+const PORT = process.env.PORT;
 
-app.get("/api/auth/login",(req,res)=>{
-    res.send("Login Route");
-});
 
-app.get("/api/auth/logout",(req,res)=>{
-    res.send("Logout Route");
-});
-
+app.use("/api/auth",authRoutes);
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
