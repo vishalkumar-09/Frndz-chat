dcfb775e9a6ebca051d5e3d8a02a49c14085d279
Vishal Kumar Gaud <iamvishalkumar09@gmail.com>
2025-06-09 13:11:47 +0530

    Set up Login & Logout Endpoint
    Login & Logout Endpoint setup

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -59,6 +59,11 @@ userSchema.pre("save",async function(next){
         next(error);
     }
 });
+
+userSchema.methods.matchPassword=async function(enteredPassword){
+    const isPasswordCorrect= await bcrypt.compare(enteredPassword,this.password);
+    return isPasswordCorrect;
+};
 const User= mongoose.model("User",userSchema);
 
 
