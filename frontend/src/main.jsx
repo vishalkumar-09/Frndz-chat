317380e9096f5a2be03e1fa711dfded67d0deb67
Shivabhadauriya3 <162864672+Shivabhadauriya3@users.noreply.github.com>
2025-06-20 23:16:26 +0530

    completed Fronted part
    HomePage & SignUp page  & Login page & Onboarding page & Added Components of Hooks folder, Constant folders, Component folder, Lib folder and parts of store theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,19 @@ import App from './App.jsx'
 
 import { BrowserRouter } from "react-router";
 
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-     <App />
+     <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
