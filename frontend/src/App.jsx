ca6d7a2cb1a15ed2b58386b65bc49c84a3c1d585
Shivabhadauriya3 <162864672+Shivabhadauriya3@users.noreply.github.com>
2025-06-20 13:41:48 +0530

    Adding Changes in Frontend
    CallPage.jsx,ChatPage.jsx,HomePage.jsx,LoginPage.jsx,NotificationPage.jsx,OnboardingPage.jsx,SignupPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+
+import toast, { Toaster } from "react-hot-toast";
+
 const App = () => {
   return (
-    <div
-      className="bg-red-500 h-screen text-xl"
-      style={{ border: "5px solid black" }}
-    >
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
+    <div className="h-screen" data-theme="dark">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+      </Routes>
+      <Toaster/>
     </div>
   );
 };
+
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // ✅ This must be here
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { BrowserRouter } from "react-router";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
