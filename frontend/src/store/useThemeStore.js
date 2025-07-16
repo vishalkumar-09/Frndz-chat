0dd380c9fd57a8c20a4d15d4161badb63c459e11
Akash Kumar Gaud <142931881+iamakashkumar09@users.noreply.github.com>
2025-07-16 12:00:41 +0530

    Update

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("streamify-theme") || "coffee",
+  theme: localStorage.getItem("Frndz-theme") || "coffee",
   setTheme: (theme) => {
-    localStorage.setItem("streamify-theme", theme);
+    localStorage.setItem("Frndz-theme", theme);
     set({ theme });
   },
 }));
